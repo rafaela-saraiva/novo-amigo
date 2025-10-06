ef22849daa06a04d6553d9956474e84fc1f97921
jao1311 <145819511+jao1311@users.noreply.github.com>
2025-10-06 16:15:36 -0300

    Teste.txt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { useEffect, useState } from "react";
+import Header from "@/components/Header";
+
 
 export default function Home() {
   return (
    <>
-   
+   <Header/>
    </>
   );
 }
