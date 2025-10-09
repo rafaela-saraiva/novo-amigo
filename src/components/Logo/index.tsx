c005be0c9e94b5f30cebdc9ce2b7a691bda15d80
caiuqemateus <133376423+caiuqemateus@users.noreply.github.com>
2025-10-09 20:29:20 -0300

    refatorar componente Logo para incluir link na imagem

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,16 +1,19 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Logo() {
     return (
         <div style={{ display: 'flex', alignItems: 'center' }}>
-            <Image
-                src="https://i.postimg.cc/43gQFF0v/560157153-858862503968347-5622696388776036153-n.png"
-                alt="Logo Novo Amigo"
-                width={250}
-                height={290}
-                priority={true}
-                style={{ objectFit: 'contain' }}
-            />
+            <Link href="/" aria-label="Ir para página inicial">
+                <Image
+                    src="https://i.postimg.cc/43gQFF0v/560157153-858862503968347-5622696388776036153-n.png"
+                    alt="Logo Novo Amigo"
+                    width={250}
+                    height={290}
+                    priority={true}
+                    style={{ objectFit: 'contain' }}
+                />
+            </Link>
         </div>
     );
-}   
+}
